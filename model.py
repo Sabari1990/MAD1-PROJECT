7f1d92259f318d09cf0e2a7c33b00397eed12087
Vijayakumar K <21f1004356@ds.study.iitm.ac.in>
2024-11-20 16:51:05 +0530

    updated at 4.45 PM on 20.11.2024

--- model.py
+++ model.py
@@ -9,10 +9,3 @@ class users(db.Model):
     password=db.Column(db.String)
     user_type=db.Column(db.String)
 
-class users(db.Model):
-    __tablename__= 'movies'
-    id= db.Column(db.Integer,primary_key=True, autoincrement = True)
-    name=db.Column(db.String)
-    email= db.Column(db.String, unique=True)
-    password=db.Column(db.String)
-    user_type=db.Column(db.String)
