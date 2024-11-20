df0c3172d8096838898498d555362f3e2f7e9d04
Vijayakumar K <21f1004356@ds.study.iitm.ac.in>
2024-11-20 12:47:53 +0530

    Data Base Created and linked

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template as rt
+from model import *
+import os
+current_dir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]= "sqlite:///"+\
+os.path.join(current_dir,"Database.sqlite3")
+
+db.init_app(app)
+app.app_context().push()
 
 app.route('/', methods=['GET','POST'])
 def home():
@@ -7,5 +15,6 @@ def home():
     
 
 if __name__ == "__main__":
+    db.create_all()
     app.debug = True
     app.run(host='0.0.0.0')
