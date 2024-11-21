b2dc712bbddf9d8495efd95d1c99be0ed197d729
Vijayakumar K <21f1004356@ds.study.iitm.ac.in>
2024-11-21 14:02:12 +0530

    query try for username start a

--- main.py
+++ main.py
@@ -9,21 +9,22 @@ os.path.join(current_dir,"Database.sqlite3")
 db.init_app(app)
 app.app_context().push()
 
-app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET','POST'])
 def home():
     return rt('home.html')
 
-app.route('/sqldemo', methods=['GET','POST'])
+@app.route('/sqldemo', methods=['GET','POST'])
 def sqldemo():
-    data = users.query.filter(users.user_type =='customer', users.name.like("a%")).all()
+    data = users.query.filter(users.name.like('a%')).all()
     print('id','name','email','user_type')
     for i in data:
         print(i.id,i.name,i.email,i.user_type)
-        return"sql_demo check vs_code terminal for output"
+    return"sql_demo check vs_code terminal for output"
     
-app.route('/datashow', methods=['GET','POST'])
+@app.route('/datashow', methods=['GET','POST'])
 def datashow():
     return rt('data.html', var="Saran")
+
     
 
     
